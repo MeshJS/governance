213e9ce4b1c79bc250cb9fbfe708bfd058652b07
André Diamond <32074058+Andre-Diamond@users.noreply.github.com>
2025-03-24 14:30:05 +0200

    Update ROOT_URL and PROJECT_GITHUB_URL to reflect new domain and repository

--- apps/docs/src/configs/index.ts
+++ apps/docs/src/configs/index.ts
@@ -1,5 +1,5 @@
-export const ROOT_URL = "https://mesh-gov.netlify.app";
+export const ROOT_URL = "https://gov.meshjs.dev/";
 
 export const APP_NAME = "Mesh Governance";
 export const APP_DESC = "Resources on Mesh Governance";
-export const PROJECT_GITHUB_URL = "https://github.com/MeshJS/cardano-governance";
+export const PROJECT_GITHUB_URL = "https://github.com/MeshJS/governance";
